90ab9c306bdb4f40ed370afd3ab70e462ec8147d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-30 20:09:43 -0300

    docs: add info about the event loop

---
+++ event-loop.js
@@ -0,0 +1,20 @@
+function shouldContinue(){
+  // Check one: any pending setTimeout, setINterval, setImmediate?
+  // Check two: any pending OS tasks? (like server listening to port)
+  // Check three: any pending long running operations? (like fs module)
+  return pendingTimers.length || pendingOSTasks.length || pendingOperations.length
+}
+
+// Entire body executes in one 'tick'
+while(shouldContinue()){
+  // 1) Node looks at pendingTimers and sees if any functions are ready to be called. setTimeout, setInterval
+  // 2) Node looks at pendingOSTasks and pendingOperations and calls relevant callbacks
+  // 3) Pause execution. Continue when...
+  // - a new pendingOSTask is done
+  // - a new pendingOperation is done
+  // - a timer is about to complete
+  // 4) Look at pendingTimers. Call any setImmediate
+  // 5) Handle any 'close' events
+}
+
+// exit back to terminal
