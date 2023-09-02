6f84b53f548c69f9855a9d69637d6ed33ed545f8
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-09-02 14:19:23 -0300

    docs: update threads file

--- main/threads.js
+++ main/threads.js
@@ -1,5 +1,11 @@
 const crypto =  require('crypto') 
 
+// Processes completed in groups of 2
+// process.env.UV_THREADPOOL_SIZE = 2 
+
+// 
+process.env.UV_THREADPOOL_SIZE = 5
+
 /**
  * Node Event Loop => Single Threaded
  * Some of Node framework/Std Lib => Not Single Threaded
@@ -11,5 +17,17 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
   console.log('2:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('3:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('4:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('5:', Date.now() - start)
 })
 
